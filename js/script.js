b78807d31ba4e27c9f0c7ebd1424d8cef4b27d28
oleoot <oleoot28@gmail.com>
2020-03-24 07:43:18 +0200

    Displaying books

--- js/script.js
+++ js/script.js
@@ -31,6 +31,18 @@ class UI {
             UI.addBookToList(book)
         })
     }
+    static addBookToList(book) {
+        const list = document.querySelector('#book-list');
+
+        const row = document.createElement('tr');
+
+        row.innerHTML = `
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.isbn}</td>
+        <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>`;
+        list.appendChild(row);
+    }
 }
 
 
@@ -40,7 +52,7 @@ class UI {
 
 
 // Event: Показать книги
-
+document.addEventListener("DOMContentLoaded", UI.displayBooks)
 
 // Event: Добавить книгу
 
